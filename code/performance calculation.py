761d99ed1643cae0f381333fda39647e3809c1b4
Jinmo <97722297+GoodJinMo@users.noreply.github.com>
2023-08-11 12:02:12 +0900

    Update performance calculation.py

--- code/performance calculation.py
+++ code/performance calculation.py
@@ -12,18 +12,20 @@ class get_info:
         self.label_path=label_path
         self.image_path=image_path
         self.model=md
-        self.truths = self.return_score()
-        self.preds=self.pred_xyxy()
+        self.truths,self.truths_cl = self.return_score()
+        self.preds,self.preds_cl=self.pred_xyxy()
         
     def return_score(self):
         names=self.files_load(self.label_path,".txt")
         truth_boxes={}
+        cl={}
         for name in names: 
             line_count = self.lines_in_file(self.label_path+"/"+name+".txt")
             truth_boxes[name]=[]
-            
+            cl[name]=[]
             for i in line_count:
                 q=i.split(" ")[1:]
+                r=i.split(" ")[0]
                 a=[float(j) for j in q]
                 w,h=self.get_image_size(self.image_path+"/"+name+".jpg")
                 xmin = (a[0]-a[2]/2)*w
@@ -31,7 +33,8 @@ class get_info:
                 ymin=(a[1]-a[3]/2)*h
                 ymax=(a[1]+a[3]/2)*h
                 truth_boxes[name].append((xmin,ymin,xmax,ymax))
-        return dict(sorted(truth_boxes.items()))
+                cl[name].append(r)
+        return dict(sorted(truth_boxes.items())),dict(sorted(cl.items()))
         
     def files_load(self,path, ext='.jpg'): #파일 안오 모든 파일 불러오기
         files = []
@@ -67,54 +70,76 @@ class get_info:
         files=self.files_load(self.image_path, ext='.jpg')
         imgs=[self.image_path+"/"+img+".jpg" for img in files]
         boxes={}
+        clss={}
         results=self.model(imgs)
         for result in results:
             name=result.path.split("/")[-1][:-4]
             boxes[name]=result.boxes.xyxy
-        return dict(sorted(boxes.items()))
+            clss[name]=result.boxes.cls
+        return dict(sorted(boxes.items())),dict(sorted(clss.items()))
     
 #%% 정밀도,재현도,f1-socre 계산
 class return_score:
-    def __init__(self,truths,preds,iou_threshold=0.5):
+    def __init__(self,truths,preds,tr_cl,pred_cl,iou_threshold=0.5):
         self.truths=truths
+        self.truths_cls=tr_cl
+        self.pred_cl=pred_cl
         self.preds=preds
         self.iou_threshold=iou_threshold
         self.score=self.calculate_recall_precision()
     def calculate_recall_precision(self):
-        true_positives = 0
-        false_positives = 0
-        false_negatives = 0
-        len_truth=0
+        true_positives = {}
+        false_positives = {}
+        false_negatives = {}
+        n={'missing': 0,'mouse': 1,'open': 2,'short': 3,'spur': 4,'spurious': 5}
+        len_truth={}
     
         for truth,pred in zip(self.truths,self.preds):
             ground_truth=self.truths[truth]
             predictions=self.preds[pred]
-            len_truth+=len(truth)
-            for pred_box in predictions:
+            
+            ground_truth_cl=self.truths_cls[truth]
+            predictions_cl=self.pred_cl[pred]
+            
+            
+            name=truth.split("_")[1]
+            name=n[name]
+            try:
+                len_truth[name]+=len(self.truths_cls[truth])
+            except:
+                true_positives[name]=0
+                false_positives[name]=0
+                len_truth[name]=len(self.truths_cls[truth])
+            
+            for pred_box,pred_cl in zip(predictions,predictions_cl):
                 pred_box_detected = False
         
-                for gt_box in ground_truth:
+                for gt_box,gt_cl in zip(ground_truth,ground_truth_cl):
                     iou = self.calculate_iou(pred_box, gt_box)
-                    if iou >= self.iou_threshold:
+                    gt=int(gt_cl)
+                    
+                    if iou >= self.iou_threshold and pred_cl==gt:
                         pred_box_detected = True
                         break
-        
+                name=truth.split("_")[1]
+                name=n[name]
                 if pred_box_detected:
-                    true_positives += 1
+                    true_positives[name] += 1
                 else:
-                    false_positives += 1
+                    false_positives[name] += 1
     
-        false_negatives = len_truth - true_positives
-    
-        recall = true_positives / (true_positives + false_negatives)
-        precision = true_positives / (true_positives + false_positives)
-        F1_score = (2*precision*recall)/(precision+recall)
-        
-        print(f"Recall: {recall:.2f}")
-        print(f"Precision: {precision:.2f}")
-        print(f"F1_score: {F1_score:.2f}")
-        
-        return recall, precision,F1_score
+        for i in len_truth:
+            t=len_truth[i]
+            tp=true_positives[i]
+            fp=false_positives[i]
+            fn = t - tp
+            recall = tp / (tp + fn)
+            precision = tp/ (tp + fp)
+            F1_score = (2*precision*recall)/(precision+recall)
+            print("============",i,"============")
+            print(f"Recall: {recall:.2f}")
+            print(f"Precision: {precision:.2f}")
+            print(f"F1_score: {F1_score:.2f}")
 
     def calculate_iou(self,box1, box2):
         # box: (x_min, y_min, x_max, y_max)
@@ -134,6 +159,7 @@ class return_score:
         iou = intersection_area / union_area
         return iou
     
+       
 #%% 실행 부분
 labels_path="C:/Users/USER/Desktop/img/test/labels" #test label 경로
 images_path="C:/Users/USER/Desktop/img/test/images" #test image 경로
@@ -142,4 +168,6 @@ info=get_info(labels_path,images_path,model)
 truths=info.truths
 preds =info.preds
 
-score = return_score(truths,preds,0.5) # (실제,예측,iou설정값)
+truths_cl=info.truths_cl
+preds_cl =info.preds_cl
+score = return_score(truths,preds,truths_cl,preds_cl,0.5) # (실제,예측,iou설정값)
